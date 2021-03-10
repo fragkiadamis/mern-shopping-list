3a5ecf356f601527a799d41d980edb27d92f9c6b
Adam Fragkiadakis <adamisfrag@hotmail.com>
2021-03-10 23:19:25 +0200

    prepare for deployment

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mezyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const { mongoURI } = require('./config/keys');
 const items = require('./routes/api/items');
 
@@ -19,6 +21,16 @@ mongoose
 
 // User routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set a static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
